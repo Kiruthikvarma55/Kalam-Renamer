4c39c107d9b6a75cbbc0cc48c069d1255ec5b52e
Kiruthikvarma55 <75213972+Kiruthikvarma55@users.noreply.github.com>
2020-12-10 13:00:16 +0530

    Update start.py

--- start.py
+++ start.py
@@ -1,5 +1,6 @@
 from pyrogram import Client, Filters, StopPropagation, InlineKeyboardButton, InlineKeyboardMarkup
 
+
 @Client.on_message(Filters.command(["start"]), group=-2)
 async def start(client, message):
     # return
@@ -8,6 +9,6 @@ async def start(client, message):
         [InlineKeyboardButton(
             "Report Bugs 😊", url="https://t.me/aryanvikash")]
     ])
-    welcomed = f"Hey {message.from_user.first_name} /help for More info"
+    welcomed = f"Hey <b>{message.from_user.first_name}</b>\n/help for More info"
     await message.reply_text(welcomed, reply_markup=joinButton)
     raise StopPropagation
